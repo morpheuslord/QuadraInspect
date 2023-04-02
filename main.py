fafbb27c3b84665ef2fa0a39fcfdd8589c881346
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2023-04-02 19:17:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
-import platform
 import argparse
-from git import Repo
 from pyfiglet import Figlet
 from subprocess import run
 from rich.console import Console
 from rich.pretty import pprint
 from rich.prompt import Prompt
 from rich.table import Table
+from config.installer import installer
+from config.androp import androp
+from config.apkleaks import apkleaks
+from config.mobfs import mobfs
 
 console = Console()
 os.chdir(os.getcwd())
@@ -29,188 +31,11 @@ def banner():
     pass
 
 
-def installer():
-    pprint("Validating OS:")
-    osp = platform.system()
-    pprint("OS FOUND: {}".format(osp))
-    command = ""
-    cmd_mob = ""
-    match osp:
-        case 'Darwin':
-            command = "python3 setup.py build && python3 setup.py install"
-            cmd_mob = "sudo bash setup.sh"
-        case 'Linux':
-            command = "python3 setup.py build && python3 setup.py install"
-            cmd_mob = "sudo bash setup.sh"
-        case 'Windows':
-            command = "python setup.py build && python setup.py install"
-            cmd_mob = ".\setup.bat"
-    andropass = 'https://github.com/koengu/AndRoPass'
-    apkleaks = 'https://github.com/dwisiswant0/apkleaks'
-    mobfs = 'https://github.com/MobSF/Mobile-Security-Framework-MobSF.git'
-    try:
-        os.makedirs('tools')
-    except FileExistsError:
-        pass
-    try:
-        os.makedirs('results')
-    except FileExistsError:
-        pass
-    try:
-        os.makedirs('pattern')
-    except FileExistsError:
-        pass
-    try:
-        os.makedirs('target')
-    except FileExistsError:
-        pass
-    os.chdir('{}/tools'.format(path))
-    twd = os.getcwd()
-    pprint("Cloning AndroPass")
-    try:
-        Repo.clone_from(andropass, '{}/andropass'.format(twd))
-    except:
-        pass
-    pprint("Cloning APKLeaks")
-    try:
-        Repo.clone_from(apkleaks, '{}/apkleaks'.format(twd))
-    except:
-        pass
-    pprint("Cloning MOBfs")
-    try:
-        Repo.clone_from(mobfs, '{}/mobfs'.format(twd))
-    except:
-        pass
-    os.chdir('{}/apkleaks'.format(twd))
-    run(command, shell=True)
-    os.chdir('{}/mobfs'.format(twd))
-    run(cmd_mob, shell=True)
-    pprint('Install Complete')
-
-
-def andropass(fn):
-    try:
-        osp = platform.system()
-        p = ""
-        match osp:
-            case 'Darwin':
-                p = "python3"
-            case 'Linux':
-                p = "python3"
-            case 'Windows':
-                p = "python"
-        rdir = os.getcwd()
-        tdir = '{}/tools/andropass/'.format(rdir)
-        tfile = '{x}/target/{y}'.format(x=rdir, y=fn)
-        while True:
-            command = Prompt.ask("ANDROPASS>> ")
-            match command:
-                case 'attack':
-                    run("{a} {b}AndRoPass.py -a {c}".format(a=p, b=tdir, c=tfile))
-                case 'help':
-                    pprint("This has no more than 1 option")
-                    table = Table(title="Tools Intigrated")
-                    table.add_column("Command")
-                    table.add_column("Details")
-                    table.add_row("attack", "To start the attack")
-                    table.add_row("quit", "Quit the program")
-                    table.add_row("return", "return to the main menu")
-                    console.print(table)
-                case 'SHOW OPTIONS':
-                    pass
-                case 'return':
-                    main()
-                case 'quit':
-                    quit()
-    except KeyboardInterrupt:
-        pprint("Ending process ...")
-
-
-def apkleaks(fn):
-    outfn = ""
-    cargs = ""
-    jsono = ""
-    rulesj = ""
-    try:
-        osp = platform.system()
-        p = ""
-        match osp:
-            case 'Darwin':
-                p = "python3"
-            case 'Linux':
-                p = "python3"
-            case 'Windows':
-                p = "python"
-        rdir = os.getcwd()
-        tdir = '{}/tools/apkleaks/'.format(rdir)
-        tfile = '{x}/target/{y}'.format(x=rdir, y=fn)
-        final_cmd = str("")
-        while True:
-            command = Prompt.ask("APKLEAKS>> ")
-            match command:
-                case 'SET pattern':
-                    promptj = Prompt.ask("Enter Prompt file >>")
-                    rulesj = '{a}/pattern/{b}'.format(a=rdir, b=promptj)
-                    final_cmd += str(" -p {}".format(rulesj))
-                case 'SET output':
-                    outfn = Prompt.ask("Enter output file Name >>")
-                    ofile = '{a}/results/{b}'.format(a=rdir, b=outfn)
-                    final_cmd += str(" -o {}".format(ofile))
-                case 'SET json-out':
-                    jsono = Prompt.ask("Enter output file Name >>")
-                    ojfile = '{a}/results/{b}'.format(a=rdir, b=jsono)
-                    final_cmd += str(" --json {}".format(ojfile))
-                case 'SET arguments':
-                    cargs = Prompt.ask("Enter arguments >>")
-                    argn = '{b}'.format(b=cargs)
-                    final_cmd += str(" -a {}".format(argn))
-                case 'attack':
-                    run("{a} {b}apkleaks.py {c} -f {d}".format(a=p,
-                        b=tdir, c=final_cmd, d=tfile))
-                case 'SHOW OPTIONS':
-                    opt = Table(title="Options SET")
-                    opt.add_column("OPTION")
-                    opt.add_column("SET Value")
-                    opt.add_row("OUTPUT FILE", str(outfn))
-                    opt.add_row("ARGUMENTS", str(cargs))
-                    opt.add_row("JSON OUTPUT", str(jsono))
-                    opt.add_row("PATTERN", str(rulesj))
-                    console.print(opt)
-                case 'help':
-                    opt = Table(title="APKLeaks Help Menu")
-                    opt.add_column("OPTION")
-                    opt.add_column("Discription")
-                    opt.add_row("SET output",
-                                "Write to file results")
-                    opt.add_row("SET arguments", "Disassembler arguments")
-                    opt.add_row("SET json-out", "For JSON output")
-                    opt.add_row("SET pattern", "custom patterns JSON Rules")
-                    opt.add_row("help", "Displays help menu")
-                    opt.add_row("return", "return to main menu")
-                    opt.add_row("quit", "quit the program")
-                    console.print(opt)
-                case 'return':
-                    main()
-                case 'quit':
-                    quit()
-    except KeyboardInterrupt:
-        pprint("Ending process ...")
-
-
-def mobfs(fn):
-    try:
-        rdir = os.getcwd()
-        tdir = '{}/tools/mobfs/'.format(rdir)
-        run('{}/run.bat 127.0.0.1:5000'.format(tdir))
-        pass
-    except KeyboardInterrupt:
-        pprint("Ending process ...")
-
-
 def main():
     command = ""
     fn = target
     banner()
+    rdir = os.getcwd()
     try:
         while True:
             command = Prompt.ask("AFrame Main>> ")
@@ -228,11 +53,11 @@ def main():
                 case 'START install_tools':
                     installer()
                 case 'START apkleaks':
-                    apkleaks(fn)
+                    apkleaks(fn, rdir)
                 case 'START mobfs':
-                    mobfs(fn)
+                    mobfs()
                 case 'START andropass':
-                    andropass(fn)
+                    androp(fn, rdir)
                 case 'SHOW banner':
                     banner()
                 case 'help':
