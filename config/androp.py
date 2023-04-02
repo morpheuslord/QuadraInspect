3163e884311d9011dcbf7a37b4c56878c8fd8779
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2023-04-02 19:18:12 +0530

    Add files via upload

---
+++ config/androp.py
@@ -0,0 +1,44 @@
+import os
+import platform
+from subprocess import run
+from rich.pretty import pprint
+from rich.prompt import Prompt
+from rich.console import Console
+from rich.table import Table
+from subprocess import run
+console = Console()
+
+def androp(fn, rdir):
+    try:
+        osp = platform.system()
+        p = ""
+        match osp:
+            case 'Darwin':
+                p = "python3"
+            case 'Linux':
+                p = "python3"
+            case 'Windows':
+                p = "python"
+        rdir = os.getcwd()
+        tdir = '{}/tools/andropass/'.format(rdir)
+        tfile = '{x}/target/{y}'.format(x=rdir, y=fn)
+        while True:
+            command = Prompt.ask("ANDROPASS>> ")
+            match command:
+                case 'attack':
+                    run("{a} {b}AndRoPass.py -a {c}".format(a=p, b=tdir, c=tfile))
+                case 'help':
+                    pprint("This has no more than 1 option")
+                    table = Table(title="Tools Intigrated")
+                    table.add_column("Command")
+                    table.add_column("Details")
+                    table.add_row("attack", "To start the attack")
+                    table.add_row("quit", "Quit the program")
+                    table.add_row("return", "return to the main menu")
+                    console.print(table)
+                case 'SHOW OPTIONS':
+                    pass
+                case 'quit':
+                    quit()
+    except KeyboardInterrupt:
+        pprint("Ending process ...")
---
+++ config/installer.py
@@ -0,0 +1,70 @@
+import os
+import platform
+from git import Repo
+from subprocess import run
+from rich.console import Console
+from rich.pretty import pprint
+
+console = Console()
+os.chdir(os.getcwd())
+path = str(os.getcwd())
+fn = ""
+
+def installer(rdir):
+    pprint("Validating OS:")
+    osp = platform.system()
+    pprint("OS FOUND: {}".format(osp))
+    command = ""
+    cmd_mob = ""
+    match osp:
+        case 'Darwin':
+            command = "python3 setup.py build && python3 setup.py install"
+            cmd_mob = "sudo bash setup.sh"
+        case 'Linux':
+            command = "python3 setup.py build && python3 setup.py install"
+            cmd_mob = "sudo bash setup.sh"
+        case 'Windows':
+            command = "python setup.py build && python setup.py install"
+            cmd_mob = ".\setup.bat"
+    andropass = 'https://github.com/koengu/AndRoPass'
+    apkleaks = 'https://github.com/dwisiswant0/apkleaks'
+    mobfs = 'https://github.com/MobSF/Mobile-Security-Framework-MobSF.git'
+    os.chdir(rdir)
+    try:
+        os.makedirs('tools')
+    except FileExistsError:
+        pass
+    try:
+        os.makedirs('results')
+    except FileExistsError:
+        pass
+    try:
+        os.makedirs('pattern')
+    except FileExistsError:
+        pass
+    try:
+        os.makedirs('target')
+    except FileExistsError:
+        pass
+    os.chdir('{}/tools'.format(path))
+    twd = os.getcwd()
+    pprint("Cloning AndroPass")
+    try:
+        Repo.clone_from(andropass, '{}/andropass'.format(twd))
+    except:
+        pass
+    pprint("Cloning APKLeaks")
+    try:
+        Repo.clone_from(apkleaks, '{}/apkleaks'.format(twd))
+    except:
+        pass
+    pprint("Cloning MOBfs")
+    try:
+        Repo.clone_from(mobfs, '{}/mobfs'.format(twd))
+    except:
+        pass
+    os.chdir('{}/apkleaks'.format(twd))
+    run(command, shell=True)
+    os.chdir('{}/mobfs'.format(twd))
+    run(cmd_mob, shell=True)
+    pprint('Install Complete')
