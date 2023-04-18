7544e6004c60a160f343b77658f47c00dcf5c703
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2023-04-18 07:18:34 +0530

    Update mobfs.py

--- config/mobfs.py
+++ config/mobfs.py
@@ -1,13 +1,22 @@
+import platform
 from rich.pretty import pprint
 from subprocess import run
 
-
-
 def mobfs(rdir):
+    osp = platform.system()
+    match osp:
+        case 'Darwin':
+            pre = ""
+            cmd = "run.sh"
+        case 'Linux':
+            pre = ""
+            cmd = "run.sh"
+        case 'Windows':
+            pre = "bash"
+            cmd = "run.bat"
     try:
         tdir = '{}/tools/mobfs/'.format(rdir)
-        run('{}/run.bat 127.0.0.1:5000'.format(tdir))
+        run('{a} {b}/{c}'.format(a=pre, b=tdir, c=cmd))
         pass
     except KeyboardInterrupt:
         pprint("Ending process ...")
-        
