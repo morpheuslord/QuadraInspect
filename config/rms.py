989085ec9025c6fa959101458e3b6ce87463c80c
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2023-04-23 00:40:55 +0530

    Update rms.py

--- config/rms.py
+++ config/rms.py
@@ -7,7 +7,7 @@ def rms(rdir):
     try:
         tdir = '{}/tools/rms/'.format(rdir)
         os.chdir(tdir)
-        run('node rms.js 127.0.0.1:5000')
+        run('node rms.js 127.0.0.1:5000', shell=True)
         pass
     except KeyboardInterrupt:
         pprint("Ending process ...")
