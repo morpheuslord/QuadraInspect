ba9f03a3891cddf0a95b76050d151f65d7546ca9
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2023-04-25 19:38:06 +0530

    Update additional.py

--- config/additional.py
+++ config/additional.py
@@ -25,21 +25,29 @@ def addinstall(rdir, opt):
         os.chdir("{}/tools".format(rdir))
         cwd = os.getcwd()
         match opt:
+            # SWITCH CASE FOR EACH TOOL
             case '1':
                 try:
                     try:
+                        # CHECK TO ENSURE THE NECESSARY PROGRAMS ARE PRESENT (IF NECESSARY)
                         print(check_output("java -version",
                                            stderr=STDOUT, shell=True).decode('utf-8'))
                     except OSError:
                         print("java not found on path")
                         quit
                     pprint('Installing APKeditor')
+                    # FILE OR REPO LINK
                     link = "https://github.com/REAndroid/APKEditor/releases/download/V1.1.8/APKEditor-1.1.8.jar"
+                    # CONFIG FILE LOCATION
                     link2 = "https://raw.githubusercontent.com/morpheuslord/QuadraInspect/main/config/apkeditor.py"
+                    # PRE DOWNLOAD PREP
                     os.mkdir('{}/apkeditor'.format(cwd))
                     os.chdir('{}/apkeditor'.format(cwd))
+                    # TOOL DOWNLOAD
                     run('wget {} -O apkeditor.jar'.format(link), shell=True)
+                    # MOVE TO CONFIGS
                     os.chdir('{}/config'.format(rdir))
+                    # CONFIG DOWNLOAD
                     run('wget {} -O apkeditor.py'.format(link2), shell=True)
                 except FileNotFoundError:
                     pprint('APKEDITOR file error')
@@ -49,7 +57,7 @@ def addinstall(rdir, opt):
     except FileNotFoundError:
         pprint('Not Able to Change DIR')
 
-
+# USE THIS FUNCTION INCASE ANY REQUIREMENTS ARE NOT MET AND MUST BE DOWNLOADED
 def requirements(opt):
     pass
 
