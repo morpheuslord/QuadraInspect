5d79bd523637e3eb25a181423c3c326bab865eb7
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2023-04-25 12:28:57 +0530

    Add files via upload

---
+++ config/additional.py
@@ -0,0 +1,64 @@
+import os
+from git import Repo
+from subprocess import run
+from subprocess import check_output, STDOUT
+from rich.pretty import pprint
+from rich.prompt import Prompt
+from rich.console import Console
+from rich.table import Table
+from subprocess import run
+console = Console()
+
+
+def addmenu():
+    table = Table(title="Tools Menu")
+    table.add_column("Number")
+    table.add_column("Name")
+    table.add_column("OS")
+    table.add_column("Depends/Program")
+    table.add_row("1", "APKeditor", "Windows/Linux/Unix", "Java")
+    console.print(table)
+
+
+def addinstall(rdir, opt):
+    try:
+        os.chdir("{}/tools".format(rdir))
+        cwd = os.getcwd()
+        match opt:
+            case '1':
+                try:
+                    try:
+                        print(check_output("java -version",
+                                           stderr=STDOUT, shell=True).decode('utf-8'))
+                    except OSError:
+                        print("java not found on path")
+                        quit
+                    pprint('Installing APKeditor')
+                    link = "https://github.com/REAndroid/APKEditor/releases/download/V1.1.8/APKEditor-1.1.8.jar"
+                    os.mkdir('{}/apkeditor'.format(cwd))
+                    os.chdir('{}/apkeditor'.format(cwd))
+                    run('wget {} -O apkeditor.jar'.format(link), shell=True)
+                except FileNotFoundError:
+                    pprint('APKEDITOR file error')
+                except:
+                    pprint('EXTERNAL errors')
+
+    except FileNotFoundError:
+        pprint('Not Able to Change DIR')
+
+
+def requirements(opt):
+    pass
+
+
+def addins(rdir):
+    while True:
+        cmd = Prompt.ask("Addins Main>> ")
+        match cmd:
+            case 'help':
+                addmenu()
+            case 'quit':
+                quit()
+            case 'install':
+                opt = Prompt.ask("Addins Main>> ")
+                addinstall(rdir, opt)
