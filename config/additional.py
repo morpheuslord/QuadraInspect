2e5e5c86d9cf8398dcaf180672d120b46a743065
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2023-05-14 12:52:05 +0530

    Update additional.py

--- config/additional.py
+++ config/additional.py
@@ -17,6 +17,8 @@ def addmenu():
     table.add_column("OS")
     table.add_column("Depends/Program")
     table.add_row("1", "APKeditor", "Windows/Linux/Unix", "Java")
+    table.add_row("2", "Backdoor-Apk-Master",
+                  "Linux/Unix (Tested Kali Linux)", "Java")
     console.print(table)
 
 
@@ -50,6 +52,54 @@ def addinstall(rdir, opt):
                         pass
                 except FileNotFoundError:
                     pprint('APKEDITOR file error')
+                except:
+                    pprint('EXTERNAL errors')
+            case '2':
+                try:
+                    try:
+                        print(check_output("java -version",
+                                           stderr=STDOUT, shell=True).decode('utf-8'))
+                    except OSError:
+                        print("java not found on path")
+                        quit
+                    try:
+                        print(check_output("msfconsole -version",
+                                           stderr=STDOUT, shell=True).decode('utf-8'))
+                    except OSError:
+                        print("metasploit not found on path")
+                        pprint("Installing Metasploit")
+                        run("curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall &&  chmod 755 msfinstall &&  sudo bash msfinstall", shell=True)
+                        quit
+                    pprint('Installing Backdoor-Apk-Master')
+                    link = "https://github.com/am6539/backdoor-apk-master"
+                    link2 = "https://github.com/morpheuslord/QuadraInspect/raw/main/config/backdoor.py"
+                    link3 = "https://bitbucket.org/JesusFreke/smali/downloads/baksmali-2.5.2.jar"
+                    link4 = "https://github.com/iBotPeaches/Apktool/releases/download/v2.7.0/apktool_2.7.0.jar"
+                    try:
+                        os.mkdir('{}/backdoor-apk-master'.format(cwd))
+                    except:
+                        pass
+                    os.chdir('{}/backdoor-apk-master'.format(cwd))
+                    run('git clone {} bak'.format(link), shell=True)
+                    d = os.getcwd()
+                    tdir = ('{}/backdoor-apk').format(d)
+                    run("sudo apt-get update && sudo apt-get upgrade -y", shell=True)
+                    run("sudo apt install default-jdk dex2jar unzip proguard zipalign", shell=True)
+                    run("wget {} -O baksmali.jar && chmod +x baksmali.jar && mv baksmali.jar baksmali && sudo cp baksmali /bin && sudo cp baksmali /usr/bin".format(link3), shell=True)
+                    run("wget {} -O apktool.jar && chmod +x apktool.jar && mv apktool.jar apktool && sudo cp apktool /bin && sudo cp apktool /usr/bin".format(link4), shell=True)
+                    run("chmod +x {}/third-party/android-string-obfuscator/lib/aso".format(tdir), shell=True)
+                    run("chmod +x {}/third-party/android-string-obfuscator/lib/*".format(tdir), shell=True)
+                    run("chmod +x {}/third-party/android-sdk-linux/build-tools/25.0.2/dx".format(tdir), shell=True)
+                    run("chmod +x {}/third-party/android-sdk-linux/build-tools/25.0.2/*".format(tdir), shell=True)
+                    run("chmod +x {}/third-party/proguard5.3.2/lib/*".format(tdir), shell=True)
+                    os.chdir('{}/config'.format(rdir))
+                    # CONFIG DOWNLOAD
+                    try:
+                        run('wget {} -O backdoor.py'.format(link2), shell=True)
+                    except:
+                        pass
+                except FileNotFoundError:
+                    pprint('BACKDOOR file error')
                 except:
                     pprint('EXTERNAL errors')
 
